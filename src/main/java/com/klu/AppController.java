1f3fccba25604fb331a08d6d440a0270534dfe28
Tejahaha <2300033461@kluniversity.in>
2025-07-28 10:17:54 +0530

    main

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.DeleteMapping;
 // import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/springapp1")
 public class AppController {
 	@Autowired
 	Service s;
